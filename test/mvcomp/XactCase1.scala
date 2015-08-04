85ae88edeef53a91844793dddc931650da4a7014
Mohammad Dashti <mdashti@gmail.com>
2015-08-04 18:58:46 +0200

    Added a sample case for a simple comparison in XactCase1

--- test/mvcomp/XactCase1.scala
+++ test/mvcomp/XactCase1.scala
@@ -1,5 +1,5 @@
 //MVCC vs. MVC3T
-package ddbt.lib.mvccvsmvc3t
+package ddbt.lib.mvcomp
 
 import ddbt.lib.util.XactImpl
 import ddbt.lib.util.XactImplSelector
@@ -7,6 +7,7 @@ import ddbt.lib.util.XactBench
 import XactBench._
 import ddbt.tpcc.loadtest.Driver
 import ddbt.tpcc.loadtest.Util
+import scala.util.Random
 import XactCase1._
 
 object XactCase1 {
@@ -17,39 +18,51 @@ object XactCase1 {
 	val TIMEOUT_T1 = 5000
 	val TIMEOUT_T2 = 5000
 
+	val RANDOM_SEED = 2015
+
 	def main(argv:Array[String]) {
 		val xactRunner = new XactBench(XactCase1Selector)
 		xactRunner.init
 		xactRunner.runBenchmark(true, argv)
 	}
 
+	val NUM_ACCOUNTS = 1000000
 }
 
 object XactCase1Selector extends XactImplSelector {
 	def select(impl: Int) = {
 		if (impl == MVCC_IMPL) {
-			XactCase1MVCC
+			(new XactCase1MVCC()).init
 		} else if (impl == MVC3T_IMPL) {
-			XactCase1MVC3T
+			(new XactCase1MVC3T()).init
 		} else {
 			throw new RuntimeException("No implementation selected.")
 		}
 	}
 
 	def seqInit(t1: Int, t2: Int, t3: Int, t4: Int, t5: Int) {
-		Util.seqInit(10000000, 10000000)
+		Util.seqInit(10000000, 1000000)
 	}
 
 	def xactNames = Array("Xact1", "Xact2")
 }
 
-object XactCase1MVCC extends XactImpl {
+class XactCase1MVCC extends XactImpl {
 	import ddbt.lib.mvconcurrent._
+
+	val tm = new TransactionManager(false)
+	val accountsTbl = new ConcurrentSHMapMVCC[Int /*Account Number*/,Tuple1[Long] /*Boolean*/]("ACCOUNTS_TBL")
+
+	val rnd = new Random(RANDOM_SEED)
+
 	def runXact(driver: Driver, t_num: Int, sequence: Int){
 		if (sequence == 0) {
 			val timeout = TIMEOUT_T1
+			val fromAcc = rnd.nextInt(NUM_ACCOUNTS)
+			val toAcc = rnd.nextInt(NUM_ACCOUNTS)
+			val amount = rnd.nextInt(5)+1
 			driver.execTransaction(t_num, sequence, timeout, XactBench.counting_on) {
-				execXact1()
+				execXact1(fromAcc,toAcc,amount)
 			}
 		} else if (sequence == 1) {
 			val timeout = TIMEOUT_T2
@@ -61,15 +74,31 @@ object XactCase1MVCC extends XactImpl {
 		}
 	}
 
-	def execXact1() = {
-		// logger.info("MVCC - execXact1")
-		Thread.sleep(200)
+	def execXact1(fromAccNum:Int, toAccNum:Int, amount:Int) = {
+		// logger.info("MVCC - execXact1 - Move Money")
+		implicit val xact = tm.begin("Xact1")
+		val fromAccBal = accountsTbl(fromAccNum)._1
+		if(fromAccBal > amount) {
+			accountsTbl(fromAccNum) = Tuple1(fromAccBal - amount)
+			accountsTbl.update(toAccNum, toAccBal => Tuple1(toAccBal._1 + amount))
+		}
+		xact.commit
 		1
 	}
 
 	def execXact2() = {
-		// logger.info("MVCC - execXact2")
-		Thread.sleep(1600)
+		// logger.info("MVCC - execXact2 - Sum Balances")
+		implicit val xact = tm.begin("Xact2")
+		val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
+		var computedSum = 0L
+		accountsTbl.foreach{ case (_,Tuple1(accBal)) =>
+			computedSum += accBal
+		}
+		if(computedSum != actualSum) {
+			logger.error("There was an error in the computation => computedSum (%d) != actualSum (%d)".format(computedSum, actualSum))
+			// throw new RuntimeException("There was an error in the computation")
+		}
+		xact.commit
 		1
 	}
 
@@ -77,16 +106,32 @@ object XactCase1MVCC extends XactImpl {
 		// commandSeq.foreach { x =>
 		// }
 		throw new UnsupportedOperationException
+	}
+
+	def init = {
+		implicit val xact = tm.begin("InitXact")
+		for(i <- 1 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
+		xact.commit
+		this
 	}
 }
 
-object XactCase1MVC3T extends XactImpl {
+class XactCase1MVC3T extends XactImpl {
 	import ddbt.lib.mvc3t._
+
+	val tm = new TransactionManager(false)
+	val accountsTbl = new ConcurrentSHMapMVC3T[Int /*Account Number*/,Tuple1[Long] /*Boolean*/]("ACCOUNTS_TBL")
+
+	val rnd = new Random(RANDOM_SEED)
+
 	def runXact(driver: Driver, t_num: Int, sequence: Int){
 		if (sequence == 0) {
 			val timeout = TIMEOUT_T1
+			val fromAcc = rnd.nextInt(NUM_ACCOUNTS)
+			val toAcc = rnd.nextInt(NUM_ACCOUNTS)
+			val amount = rnd.nextInt(5)+1
 			driver.execTransaction(t_num, sequence, timeout, XactBench.counting_on) {
-				execXact1()
+				execXact1(fromAcc,toAcc,amount)
 			}
 		} else if (sequence == 1) {
 			val timeout = TIMEOUT_T2
@@ -98,15 +143,30 @@ object XactCase1MVC3T extends XactImpl {
 		}
 	}
 
-	def execXact1() = {
-		// logger.info("MVC3T - execXact1")
-		Thread.sleep(100)
+	def execXact1(fromAccNum:Int, toAccNum:Int, amount:Int) = {
+		// logger.info("MVCC - execXact1 - Move Money")
+		implicit val xact = tm.begin("Xact1")
+		val fromAccBal = accountsTbl(fromAccNum)._1
+		if(fromAccBal > amount) {
+			accountsTbl(fromAccNum) = Tuple1(fromAccBal - amount)
+			accountsTbl.update(toAccNum, toAccBal => Tuple1(toAccBal._1 + amount))
+		}
+		xact.commit
 		1
 	}
 
 	def execXact2() = {
-		// logger.info("MVC3T - execXact2")
-		Thread.sleep(200)
+		// logger.info("MVCC - execXact2 - Sum Balances")
+		implicit val xact = tm.begin("Xact2")
+		val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
+		var computedSum = 0L
+		accountsTbl.foreach{ case (_,Tuple1(accBal)) =>
+			computedSum += accBal
+		}
+		if(computedSum != actualSum) {
+			throw new RuntimeException("There was an error in the computation")
+		}
+		xact.commit
 		1
 	}
 
@@ -114,5 +174,12 @@ object XactCase1MVC3T extends XactImpl {
 		// commandSeq.foreach { x =>
 		// }
 		throw new UnsupportedOperationException
+	}
+
+	def init = {
+		implicit val xact = tm.begin("InitXact")
+		for(i <- 1 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
+		xact.commit
+		this
 	}
 }
