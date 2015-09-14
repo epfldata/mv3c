ef3489e8a5513281b17bfecd74f8544f9bfc058f
Mohammad Dashti <mdashti@gmail.com>
2015-09-14 02:49:40 +0200

    Modified XactCase1 to reflect some changes in the impl + Nitz

--- test/mvcomp/XactCase1.scala
+++ test/mvcomp/XactCase1.scala
@@ -49,9 +49,10 @@ object XactCase1Selector extends XactImplSelector {
 
 class XactCase1MVCC extends XactImpl {
 	import ddbt.lib.mvconcurrent._
+	import TransactionManager._
+	val accountsTbl = new ConcurrentSHMapMVCC[Int /*Account Number*/,Tuple1[Long] /*Boolean*/]("ACCOUNTS_TBL")
 
 	val tm = new TransactionManager(false)
-	val accountsTbl = new ConcurrentSHMapMVCC[Int /*Account Number*/,Tuple1[Long] /*Boolean*/]("ACCOUNTS_TBL")
 
 	val rnd = new Random(RANDOM_SEED)
 
@@ -77,29 +78,62 @@ class XactCase1MVCC extends XactImpl {
 	def execXact1(fromAccNum:Int, toAccNum:Int, amount:Int) = {
 		// logger.info("MVCC - execXact1 - Move Money")
 		implicit val xact = tm.begin("Xact1")
-		val fromAccBal = accountsTbl(fromAccNum)._1
-		if(fromAccBal > amount) {
-			accountsTbl(fromAccNum) = Tuple1(fromAccBal - amount)
-			accountsTbl.update(toAccNum, toAccBal => Tuple1(toAccBal._1 + amount))
+		try {
+			val fromAcc = accountsTbl.getEntry(fromAccNum)
+			fromAcc.setEntryValue(Tuple1(fromAcc.row._1 - amount))
+
+			val toAcc = accountsTbl.getEntry(toAccNum)
+			toAcc.setEntryValue(Tuple1(toAcc.row._1 + amount))
+			xact.commit
+			1
+		} catch {
+			case me: MVCCException => {
+				error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				error(me)
+				tm.rollback
+				0
+			}
+			case e: Throwable => {
+				logger.error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				e.printStackTrace
+				logger.error(e.toString)
+				if(e.getStackTrace.isEmpty) logger.error("Stack Trace is empty!") else e.getStackTrace.foreach(st => logger.error(st.toString))
+				tm.rollback
+				0
+			}
 		}
-		xact.commit
-		1
 	}
 
 	def execXact2() = {
 		// logger.info("MVCC - execXact2 - Sum Balances")
 		implicit val xact = tm.begin("Xact2")
-		val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
-		var computedSum = 0L
-		accountsTbl.foreach{ case (_,Tuple1(accBal)) =>
-			computedSum += accBal
+		try {
+			val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
+			var computedSum = 0L
+			accountsTbl.foreach{ case (_,Tuple1(accBal)) =>
+				computedSum += accBal
+			}
+			if(computedSum != actualSum) {
+				logger.error("There was an error in the computation => computedSum (%d) != actualSum (%d)".format(computedSum, actualSum))
+				// throw new RuntimeException("There was an error in the computation")
+			}
+			xact.commit
+			1
+		} catch {
+			case me: MVCCException => {
+				error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				error(me)
+				tm.rollback
+				0
+			}
+			case e: Throwable => {
+				logger.error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				logger.error(e.toString)
+				if(e.getStackTrace.isEmpty) logger.error("Stack Trace is empty!") else e.getStackTrace.foreach(st => logger.error(st.toString))
+				tm.rollback
+				0
+			}
 		}
-		if(computedSum != actualSum) {
-			logger.error("There was an error in the computation => computedSum (%d) != actualSum (%d)".format(computedSum, actualSum))
-			// throw new RuntimeException("There was an error in the computation")
-		}
-		xact.commit
-		1
 	}
 
 	def runXactSeq(driver: Driver, commandSeq:Seq[ddbt.lib.util.XactCommand]): Unit = {
@@ -110,7 +144,7 @@ class XactCase1MVCC extends XactImpl {
 
 	def init = {
 		implicit val xact = tm.begin("InitXact")
-		for(i <- 1 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
+		for(i <- 0 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
 		xact.commit
 		this
 	}
@@ -118,18 +152,20 @@ class XactCase1MVCC extends XactImpl {
 
 class XactCase1MVC3T extends XactImpl {
 	import ddbt.lib.mvc3t._
-
-	val tm = new TransactionManager(false)
+	import TransactionManager._
 	val accountsTbl = new ConcurrentSHMapMVC3T[Int /*Account Number*/,Tuple1[Long] /*Boolean*/]("ACCOUNTS_TBL")
 
-	val rnd = new Random(RANDOM_SEED)
+	val tm = new TransactionManager(false)
 
+	val rnd = new Random(RANDOM_SEED)
+	var amt = 0
 	def runXact(driver: Driver, t_num: Int, sequence: Int){
 		if (sequence == 0) {
 			val timeout = TIMEOUT_T1
 			val fromAcc = rnd.nextInt(NUM_ACCOUNTS)
 			val toAcc = rnd.nextInt(NUM_ACCOUNTS)
-			val amount = rnd.nextInt(5)+1
+			amt += 1
+			val amount = amt
 			driver.execTransaction(t_num, sequence, timeout, XactBench.counting_on) {
 				execXact1(fromAcc,toAcc,amount)
 			}
@@ -146,28 +182,74 @@ class XactCase1MVC3T extends XactImpl {
 	def execXact1(fromAccNum:Int, toAccNum:Int, amount:Int) = {
 		// logger.info("MVCC - execXact1 - Move Money")
 		implicit val xact = tm.begin("Xact1")
-		val fromAccBal = accountsTbl(fromAccNum)._1
-		if(fromAccBal > amount) {
-			accountsTbl(fromAccNum) = Tuple1(fromAccBal - amount)
-			accountsTbl.update(toAccNum, toAccBal => Tuple1(toAccBal._1 + amount))
+		try {
+			if(fromAccNum != toAccNum) {
+				// logger.error(xact+" > Move %d from (%d) to (%d).".format(amount, fromAccNum, toAccNum))
+				accountsTbl.getPred(fromAccNum).getEntry({ case (fromAcc, getPred) =>
+					accountsTbl.updatePred(fromAccNum).action(fromAcc, {
+						// logger.error(xact+" AccFrom #%d = %d -> %d".format(fromAcc.getKey, fromAcc.row._1, fromAcc.row._1 - amount))
+						Tuple1(fromAcc.row._1 - amount)
+					}, List(getPred))
+				}, Nil)
+				accountsTbl.getPred(toAccNum).getEntry({ case (toAcc, getPred) =>
+					accountsTbl.updatePred(toAccNum).action(toAcc, {
+						// logger.error(xact+" AccTo   #%d = %d -> %d".format(toAcc.getKey, toAcc.row._1, toAcc.row._1 + amount))
+						Tuple1(toAcc.row._1 + amount)
+					}, List(getPred))
+				}, Nil)
+			}
+			xact.commit
+			1
+		} catch {
+			case me: MVCCException => {
+				error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				error(me)
+				tm.rollback
+				0
+			}
+			case e: Throwable => {
+				logger.error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				logger.error(e.toString)
+				if(e.getStackTrace.isEmpty) logger.error("Stack Trace is empty!") else e.getStackTrace.foreach(st => logger.error(st.toString))
+				tm.rollback
+				0
+			}
 		}
-		xact.commit
-		1
 	}
 
 	def execXact2() = {
 		// logger.info("MVCC - execXact2 - Sum Balances")
 		implicit val xact = tm.begin("Xact2")
-		val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
-		var computedSum = 0L
-		accountsTbl.foreach{ case (_,Tuple1(accBal)) =>
-			computedSum += accBal
+		try {
+			val actualSum = 3L * (NUM_ACCOUNTS * (NUM_ACCOUNTS - 1L)) / 2L
+			var computedSum = 0L
+			accountsTbl.foreachPred.apply({ case (dv, _) =>
+				val accBal = dv.row._1
+				// logger.error(xact+" Acc #%d = %d".format(dv.getKey, dv.row._1))
+				computedSum += accBal
+			}, Nil)
+			if(computedSum != actualSum) {
+				logger.error(xact+" > There was an error in the computation => computedSum (%d) != actualSum (%d) -> difference is %d".format(computedSum, actualSum, Math.abs(computedSum - actualSum)))
+				System.exit(0)
+				// throw new RuntimeException("There was an error in the computation")
+			}
+			xact.commit
+			1
+		} catch {
+			case me: MVCCException => {
+				error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				error(me)
+				tm.rollback
+				0
+			}
+			case e: Throwable => {
+				logger.error("Thread"+Thread.currentThread().getId()+" :> "+xact+": An error occurred")
+				logger.error(e.toString)
+				if(e.getStackTrace.isEmpty) logger.error("Stack Trace is empty!") else e.getStackTrace.foreach(st => logger.error(st.toString))
+				tm.rollback
+				0
+			}
 		}
-		if(computedSum != actualSum) {
-			throw new RuntimeException("There was an error in the computation")
-		}
-		xact.commit
-		1
 	}
 
 	def runXactSeq(driver: Driver, commandSeq:Seq[ddbt.lib.util.XactCommand]): Unit = {
@@ -178,7 +260,7 @@ class XactCase1MVC3T extends XactImpl {
 
 	def init = {
 		implicit val xact = tm.begin("InitXact")
-		for(i <- 1 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
+		for(i <- 0 until NUM_ACCOUNTS) accountsTbl += (i, Tuple1(i*3L))
 		xact.commit
 		this
 	}
