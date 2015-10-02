31a304e6739dc6f78da044373200062423db1d1c
Mohammad Dashti <mdashti@gmail.com>
2015-10-03 00:56:50 +0200

    Updated hyper-threading script

--- bench/prep/hyper-threading.sh
+++ bench/prep/hyper-threading.sh
@@ -2,14 +2,25 @@
 
 [ -z "$1" ] && echo "$0 1|0" && exit 1
 
-# Be careful to not skip the space at the beginning nor the end
-CPUS_TO_SKIP=" $(cat /sys/devices/system/cpu/cpu*/topology/thread_siblings_list | sed 's/[^0-9].*//' | sort | uniq | tr "\r\n" "  ") "
+for CPU in /sys/devices/system/cpu/cpu[0-9]*; do
+    CPUID=$(basename $CPU)
+    echo "CPU: $CPUID";
+    if test -e $CPU/online; then
+            echo "1" > $CPU/online; 
+            if [ "$1" = "1" ]; then
+            	echo "${CPU} core=${COREID} -> enable"
+            fi;
+    fi;
 
-
-for CPU_PATH in /sys/devices/system/cpu/cpu[0-9]*; do
-    CPU="$(echo $CPU_PATH | tr -cd "0-9")"
-    echo "$CPUS_TO_SKIP" | grep " $CPU " > /dev/null
-    if [ $? -ne 0 ]; then
-        echo $1 > $CPU_PATH/online
-    fi
-done
+    if [ "$1" = "0" ]; then
+        COREID="$(cat $CPU/topology/core_id)";
+        eval "COREENABLE=\"\${core${COREID}enable}\"";
+        if ${COREENABLE:-true}; then        
+                echo "${CPU} core=${COREID} -> enable"
+                eval "core${COREID}enable='false'";
+        else
+                echo "$CPU core=${COREID} -> disable"; 
+                echo "0" > "$CPU/online"; 
+        fi;
+    fi;
+done;
