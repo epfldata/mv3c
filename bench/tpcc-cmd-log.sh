f5d401576c94fb820fd219c8d9549137d77829dd
Mohammad Dashti <mdashti@gmail.com>
2017-09-11 00:28:16 +0200

    Fixed the TPC-C command generator to accept parameters

--- bench/tpcc-cmd-log.sh
+++ bench/tpcc-cmd-log.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-WAREHOUSES="2"
-NUMXACTS="100"
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 WAREHOUSES NUMXACTS OUTPUTFILE"
+    exit 1
+fi
+
+WAREHOUSES=$1
+NUMXACTS=$2
 
 cd `dirname $0`; BENCH=`pwd`; cd ..; BASE=`pwd`
 
-eval "bench/bench load $WAREHOUSES"
-
 #eval "sbt 'run-main ddbt.tpcc.loadtest.TpccCommandGen -w $WAREHOUSES -n $NUMXACTS'"
+
 eval "sbt package"
 
-# eval "scala -cp .:./lib/*:./target/scala-2.11/tstore_2.11-0.1.jar ddbt.tpcc.loadtest.TpccCommandGen -w $WAREHOUSES -n $NUMXACTS"
+echo "Creating $NUMXACTS commands for $WAREHOUSES warehouses."
 
-eval "scala -cp .:./lib/*:./target/scala-2.11/tstore_2.11-0.1.jar ddbt.tpcc.loadtest.TpccUnitTest -i -2 -w $WAREHOUSES -n $NUMXACTS >/dev/null"
+eval "scala -cp .:./lib/*:./target/scala-2.11/tstore_2.11-0.1.jar ddbt.tpcc.loadtest.TpccCommandGen -w $WAREHOUSES -n $NUMXACTS" > $3
 
-eval "bench/bench save $WAREHOUSES"
+echo "The commands are generated in $3."
