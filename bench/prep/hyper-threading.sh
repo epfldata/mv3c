2a17bc058cdcca3ac7cb575f8be7241b6068398a
Mohammad Dashti <mdashti@gmail.com>
2017-09-11 14:48:53 +0200

    Improved hyper-threading.h

--- bench/prep/hyper-threading.sh
+++ bench/prep/hyper-threading.sh
@@ -2,25 +2,30 @@
 
 [ -z "$1" ] && echo "$0 1|0" && exit 1
 
-for CPU in /sys/devices/system/cpu/cpu[0-9]*; do
+CTR=0
+
+for CPUFILE in /sys/devices/system/cpu/cpu[0-9]*; do
+    CPU="/sys/devices/system/cpu/cpu$CTR"
     CPUID=$(basename $CPU)
     echo "CPU: $CPUID";
     if test -e $CPU/online; then
-            echo "1" > $CPU/online; 
+            echo "1" > $CPU/online;
             if [ "$1" = "1" ]; then
-            	echo "${CPU} core=${COREID} -> enable"
+                echo "${CPU} core=${COREID} -> enable"
             fi;
     fi;
 
     if [ "$1" = "0" ]; then
         COREID="$(cat $CPU/topology/core_id)";
-        eval "COREENABLE=\"\${core${COREID}enable}\"";
-        if ${COREENABLE:-true}; then        
-                echo "${CPU} core=${COREID} -> enable"
-                eval "core${COREID}enable='false'";
+        PHYSICALID="$(cat $CPU/topology/physical_package_id)";
+        eval "COREENABLE=\"\${core${PHYSICALID}AND${COREID}enable}\"";
+        if ${COREENABLE:-true}; then
+                echo "${CPU} physical=${PHYSICALID} core=${COREID} -> enable"
+                eval "core${PHYSICALID}AND${COREID}enable='false'";
         else
-                echo "$CPU core=${COREID} -> disable"; 
-                echo "0" > "$CPU/online"; 
+                echo "$CPU physical=${PHYSICALID} core=${COREID} -> disable";
+                echo "0" > "$CPU/online";
         fi;
     fi;
+    CTR=$((CTR+1))
 done;
