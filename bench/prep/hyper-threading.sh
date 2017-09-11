437052c837beb2d2c6f79a1cf27808bb367b1ca8
Sachin Basil John <sachinbjohn@gmail.com>
2017-09-11 16:08:27 +0200

    Changed hyperthreading script

--- bench/prep/hyper-threading.sh
+++ bench/prep/hyper-threading.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 [ -z "$1" ] && echo "$0 1|0" && exit 1
-
+declare -a ENABLEDCORES
 CTR=0
-
+NUMCORES=0
+PHYSICALID_FIRST="$(cat /sys/devices/system/cpu/cpu0/topology/physical_package_id)";
 for CPUFILE in /sys/devices/system/cpu/cpu[0-9]*; do
     CPU="/sys/devices/system/cpu/cpu$CTR"
     CPUID=$(basename $CPU)
-    echo "CPU: $CPUID";
+    echo -n "CPU: $CPUID";
     if test -e $CPU/online; then
             echo "1" > $CPU/online;
             if [ "$1" = "1" ]; then
                 echo "${CPU} core=${COREID} -> enable"
+                NUMCORES=$((NUMCORES+1))
             fi;
     fi;
 
@@ -19,13 +21,35 @@ for CPUFILE in /sys/devices/system/cpu/cpu[0-9]*; do
         COREID="$(cat $CPU/topology/core_id)";
         PHYSICALID="$(cat $CPU/topology/physical_package_id)";
         eval "COREENABLE=\"\${core${PHYSICALID}AND${COREID}enable}\"";
-        if ${COREENABLE:-true}; then
-                echo "${CPU} physical=${PHYSICALID} core=${COREID} -> enable"
+        if  ${COREENABLE:-true} && [ $PHYSICALID = $PHYSICALID_FIRST ]; then
+                echo " physical=${PHYSICALID} core=${COREID} -> enable"
                 eval "core${PHYSICALID}AND${COREID}enable='false'";
+                cpufreq-set -g performance -c $CTR 
+                NUMCORES=$((NUMCORES+1))
+                ENABLEDCORES+=($CTR)
         else
-                echo "$CPU physical=${PHYSICALID} core=${COREID} -> disable";
+                echo " physical=${PHYSICALID} core=${COREID} -> disable";
                 echo "0" > "$CPU/online";
         fi;
     fi;
     CTR=$((CTR+1))
 done;
+
+echo "Total number of enabled cores = $NUMCORES"
+
+FLAG1=1
+FLAG2=1
+FLAG4=1
+
+for c in `seq $NUMCORES`
+do
+    i=$((c-1))
+    FLAG1=$((FLAG1 && ENABLEDCORES[i]==i))
+    FLAG2=$((FLAG2 && ENABLEDCORES[i]==2*i))
+    FLAG4=$((FLAG4 && ENABLEDCORES[i]==4*i))
+done
+
+export FLAG1
+export FLAG2
+export FLAG4
+export NUMCORES
